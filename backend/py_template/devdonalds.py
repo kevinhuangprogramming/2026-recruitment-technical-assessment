bc922f1ca3cb2d889f39396b664de324c1ab4385
Kevin Huang <kevin.j.huang@student.unsw.edu.au>
2026-02-27 12:01:42 +1100

    Completed Python function for Task 2

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -28,7 +28,7 @@ class Ingredient(CookbookEntry):
 app = Flask(__name__)
 
 # Store your recipes here!
-cookbook = None
+cookbook = Dict[str, Union[Recipe, Ingredient]] = {}
 
 # Task 1 helper (don't touch)
 @app.route("/parse", methods=['POST'])
@@ -77,8 +77,80 @@ def parse_handwriting(recipeName: str) -> Union[str | None]:
 # Endpoint that adds a CookbookEntry to your magical cookbook
 @app.route('/entry', methods=['POST'])
 def create_entry():
-	# TODO: implement me
-	return 'not implemented', 500
+	data = request.get_json()
+
+	success = add_cookbook_entry(data)
+
+	if not success:
+		return '', 400
+
+	return '', 200
+
+
+# Backend function
+def add_cookbook_entry(data: dict) -> bool:
+	global cookbook
+
+	# Basic validation
+	if not isinstance(data, dict):
+		return False
+
+	name = data.get("name")
+	entry_type = data.get("type")
+
+	if not isinstance(name, str) or not isinstance(entry_type, str):
+		return False
+
+	# Type must be recipe or ingredient
+	if entry_type not in ["recipe", "ingredient"]:
+		return False
+
+	# Name must be unique
+	if name in cookbook:
+		return False
+
+
+	# Testing ingredient...
+	if entry_type == "ingredient":
+		cook_time = data.get("cookTime")
+
+		if not isinstance(cook_time, int) or cook_time < 0:
+			return False
+		cookbook[name] = Ingredient(name=name, cook_time=cook_time)
+		return True
+
+
+	# Testing recipe...
+	if entry_type == "recipe":
+		required_items = data.get("requiredItems")
+
+		if not isinstance(required_items, list):
+			return False
+
+		seen_names = set()
+		parsed_items: List[RequiredItem] = []
+
+		for item in required_items:
+			if not isinstance(item, dict):
+				return False
+
+			item_name = item.get("name")
+			quantity = item.get("quantity")
+
+			if not isinstance(item_name, str) or not isinstance(quantity, int):
+				return False
+
+			# Only one element per name
+			if item_name in seen_names:
+				return False
+
+			seen_names.add(item_name)
+			parsed_items.append(RequiredItem(name=item_name, quantity=quantity))
+
+		cookbook[name] = Recipe(name=name, required_items=parsed_items)
+		return True
+
+	return False
 
 
 # [TASK 3] ====================================================================
